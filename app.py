8852318814ab2a71de0c1a5cb25abace024efd45
alihuss1071 <alihuss1017@gmail.com>
2024-01-31 21:30:58 -0800

    [ADD] webpage title

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ transform = transforms.Compose([
               transforms.ToTensor(), 
               transforms.Normalize(mean = [0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
+st.set_page_config("Ali's Pokedex")
+
 st.title("Ali's Pokedex")
 
 img = st.file_uploader('Upload image of Pokemon: ', type = ['jpeg', 'jpg', 'png'])
